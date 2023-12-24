f3e43eb5f7672aca6454781ec326d37529dd7644
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2023-12-23 20:32:39 -0700

    Define types with interfaces for Community and Home

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+// export enum HomeType {
+//     House = "House",
+//     Townhome = "Townhome",
+//     Duplex = "Duplex",
+//     Condo = "Condo"
+// }
+
+export type HomeType = "House" | "Townhouse" | "Duplex" | "Conde";
+
+export interface Community {
+  id: string;
+  name: string;
+  imageUrl: string;
+  group: string;
+}
+
+export interface Home {
+  id: string;
+  communityId: string;
+  price: number;
+  area: number;
+  type: HomeType;
+}
