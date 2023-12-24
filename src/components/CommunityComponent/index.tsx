48df2c1e4a6fdacce6d6455b97fbc6f67217c3c7
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2023-12-24 09:22:51 -0700

    Update price display logic using Intl.NumberFormat

--- src/components/CommunityComponent/index.tsx
+++ src/components/CommunityComponent/index.tsx
@@ -11,34 +11,57 @@ interface Props {
 const CommunityComponent: FC<Props> = ({ community, homes }) => {
   const [averagePriceInCommunity, setAveragePriceInCommunity] =
     useState<number>(0);
-  const [formattedPriceInCommunity, setFormattedPriceInCommunity] =
-    useState<string>();
+  const [
+    formattedAveragePriceInCommunity,
+    setFormattedAveragePriceInCommunity,
+  ] = useState<string>();
 
   // Calculate average price of all homes associated with that community
   const calculateAveragePrice = useCallback((homes: Home[]): number => {
+    if (homes.length === 0) {
+      return 0;
+    }
     const totalPrice = homes.reduce((sum, home) => sum + home.price, 0);
-    const averagePrice = totalPrice / homes.length;
-    return averagePrice;
+    return totalPrice / homes.length;
   }, []);
 
   // Format price with currency, and handle unexpected value
-  const formatAveragePrice = useCallback(() => {}, []);
+  const formatAveragePrice = useCallback((averagePrice: number): string => {
+    if (averagePrice === 0) {
+      return "N/A";
+    }
+
+    return new Intl.NumberFormat("en-CA", {
+      style: "currency",
+      currency: "CAD",
+    }).format(averagePrice);
+  }, []);
 
   useEffect(() => {
-    // Call calculateAveragePrice function to calculate average price
-    setAveragePriceInCommunity(calculateAveragePrice(homes));
-    formatAveragePrice();
+    // Get calculateAveragePrice function to calculate average price
+    const averagePrice = calculateAveragePrice(homes);
+    setAveragePriceInCommunity(averagePrice);
+
+    const formattedAveragePrice = formatAveragePrice(averagePrice);
+    setFormattedAveragePriceInCommunity(formattedAveragePrice);
+    // setAveragePriceInCommunity(calculateAveragePrice(homes));
+    // formatAveragePrice();
     console.log("community: ", community);
     console.log("homes", homes);
-  }, [homes, calculateAveragePrice]);
+  }, [homes, calculateAveragePrice, formatAveragePrice, community]);
 
   return (
     <li>
       <ul>Community Name: {community.name}</ul>
       <ul>
-        <img src={community.imgUrl} alt={community.name} />
+        <img
+          src={community.imgUrl}
+          alt={community.name}
+          width={300}
+          height={300}
+        />
       </ul>
-      <ul>Average price: {averagePriceInCommunity}</ul>
+      <ul>Average price: {formattedAveragePriceInCommunity}</ul>
     </li>
   );
 };
